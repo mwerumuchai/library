bf2700d816b19272728207c4c00aece8a3329d1b
mwerumuchai <mwerumuchai@gmail.com>
2018-03-08 08:56:05 +0300

    COnsole Application

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Object obj = new Object();
+            obj = ("Reg no: CS/20/2016, booK: PE 1478 S65 1991, DATE: 21/01/2016-30/01/2016 b.");
+            Console.WriteLine(obj.ToString());
+        }
+    }
+}
